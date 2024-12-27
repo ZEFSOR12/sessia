d6b6726c122e6147e84a5797bbeb7905d515a9ab
ZEFSOR12 <164011315+ZEFSOR12@users.noreply.github.com>
2024-12-27 08:23:56 +0300

    Update UnitTest1.cs

--- WpfApp1/WpfApp1/UnitTest1.cs
+++ WpfApp1/WpfApp1/UnitTest1.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using ClassLibrary2;
+using имя проекта;
 
 namespace WpfApp1
 {
@@ -25,19 +25,34 @@ namespace WpfApp1
         }
         public static int ResultUnit2 { get; set; }
         [TestMethod]
-        public void TestDateOkonch(string Date)
+
+        //class1
+     public static bool CheckForNil(string pass, string email)
+    {
+        bool isNull = true;
+        if (!string.IsNullOrEmpty(pass) || !string.IsNullOrEmpty(email))
         {
-            string DateTest = Date;
-            bool expected = false;
-            Class1 g = new Class1();
-            bool result = g.Test1(DateTest);
-            try
-            {
-                Assert.AreEqual(expected, result);
-                ResultUnit2 = 1;
-            }
-            catch (Exception ex)
-            { ResultUnit2 = 0; }
+            isNull = false;
         }
+        return isNull;
+    }
+
+//tescunit
+
+        public void Test1()
+{
+    [Fact]
+    public void TestForNull()
+    {
+        bool result = Class1.CheckForNil("123", null);
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void TestForeEmpty()
+    {
+        bool result = Class1.CheckForNil("123", "");
+        Assert.False(result);
+    }
     }
 }
